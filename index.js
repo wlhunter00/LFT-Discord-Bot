43d8685b49cbf54205875d11d9b56ca3fa5cf2f1
William Hunter <30416118+wlhunter00@users.noreply.github.com>
2021-01-11 17:58:26 -0500

    setup config file

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 require('dotenv').config();
 
 const Discord = require('discord.js');
+const config = require('./config.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
     console.log('Ready!');
 });
 
+// Listen for a message
 client.on('message', message => {
-    console.log(message);
-})
+    console.log(message.content);
+
+    if (message.content === "!ping") {
+        // Responding to messages
+        message.channel.send("Pong.");
+    }
+});
 
 client.login(process.env.DISCORD_TOKEN);
