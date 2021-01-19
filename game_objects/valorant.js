ed708210950c77bd525da0ae9761fea8e7eb8f4d
William Hunter <30416118+wlhunter00@users.noreply.github.com>
2021-01-18 19:07:54 -0500

    created valorant object

---
+++ game_objects/valorant.js
@@ -0,0 +1,86 @@
+const valorant = {
+    name: 'valorant',
+    ranks: [
+        {
+            name: 'bronze',
+            tiers: ['1', '2', '3'],
+            aliases: ['bronz', 'br', 'b']
+        },
+        {
+            name: 'silver',
+            tiers: ['1', '2', '3'],
+            aliases: ['silv', 'sil', 's']
+        },
+        {
+            name: 'gold',
+            tiers: ['1', '2', '3'],
+            aliases: ['gl', 'gd', 'go', 'g']
+        },
+        {
+            name: 'platinum',
+            tiers: ['1', '2', '3'],
+            aliases: ['plat', 'p']
+        },
+        {
+            name: 'diamond',
+            tiers: ['1', '2', '3'],
+            aliases: ['dia', 'd']
+        },
+        {
+            name: 'immortal',
+            tiers: ['1', '2', '3'],
+            aliases: ['immo', 'imm', 'i']
+        },
+        {
+            bracket: 'radiant',
+            tiers: ['1', '2', '3'],
+            aliases: ['rad', 'r']
+        },
+    ],
+    characters: [
+        {
+            name: 'brimstone',
+            aliases: ['brim']
+        },
+        {
+            name: 'omen',
+            aliases: []
+        },
+        {
+            name: 'viper',
+            aliases: []
+        }
+    ],
+    roles: [
+        {
+            name: 'entry',
+            aliases: []
+        },
+        {
+            name: 'lurker',
+            aliases: ['lurk']
+        },
+        {
+            name: "operator",
+            aliases: ['op', 'oper', 'awp']
+        },
+        {
+            name: 'controller',
+            aliases: ['']
+        },
+        {
+            name: 'initator',
+            aliases: ['']
+        },
+        {
+            name: 'duelist',
+            aliases: ['']
+        },
+        {
+            name: 'sentinel',
+            aliases: ['']
+        },
+    ],
+}
+
+exports.valorant = valorant;
