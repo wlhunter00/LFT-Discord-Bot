7313787d539c2af0dc4ee8c207447277a1ac8ffd
wamaue <wamaue@vt.edu>
2021-01-25 20:23:15 -0500

    just wrote some small loops for rank and role to test but still need to add file

--- message_parse.js
+++ message_parse.js
@@ -1,6 +1,28 @@
+const { getGame, getRanks } = require("./helper_functions/games");
+
 const parseMessageFunction = (message) => {
   message.channel.send("LFT Message Found.");
   console.log(message.content);
+
+  //is message still in paragraph or is it just giant string.
+  //if still paragraph we can check for negatives in the lines
+  //that they mention ranks to tell if they do or don't want these ranks
+
+  //Want to get the array of ranks...might not be how you are supposed to use this function
+  let arrRanks = getRanks();
+  for (const rank of arrRanks) {
+    if (message.includes(rank.name)) {
+      console.log(rank.name);
+    }
+  }
+
+  //Want to get the array of ranks...might not be how you are supposed to use this function
+  let arrRoles = getRoles();
+  for (const role of arrRoles) {
+    if (message.includes(role.name)) {
+      console.log(role.name);
+    }
+  }
 };
 
 exports.parseMessage = parseMessageFunction;
