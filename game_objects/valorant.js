743c6f1800c581c0b076e04253cdb6eb8ffc56a0
wamaue <wamaue@vt.edu>
2021-01-27 00:13:29 -0500

    Valorant game object format was changed when I saved a small change. I have it search for ranks, roles, and characters in the LFT and print out what it collects in the console

--- game_objects/valorant.js
+++ game_objects/valorant.js
@@ -1,125 +1,125 @@
 module.exports = {
-    name: 'valorant',
-    id: '1',
-    ranks: [
-        {
-            name: 'bronze',
-            tiers: ['1', '2', '3'],
-            aliases: ['bronz', 'br', 'b']
-        },
-        {
-            name: 'silver',
-            tiers: ['1', '2', '3'],
-            aliases: ['silv', 'sil', 's']
-        },
-        {
-            name: 'gold',
-            tiers: ['1', '2', '3'],
-            aliases: ['gl', 'gd', 'go', 'g']
-        },
-        {
-            name: 'platinum',
-            tiers: ['1', '2', '3'],
-            aliases: ['plat', 'p']
-        },
-        {
-            name: 'diamond',
-            tiers: ['1', '2', '3'],
-            aliases: ['dia', 'd']
-        },
-        {
-            name: 'immortal',
-            tiers: ['1', '2', '3'],
-            aliases: ['immo', 'imm', 'i']
-        },
-        {
-            bracket: 'radiant',
-            tiers: ['1', '2', '3'],
-            aliases: ['rad', 'r']
-        },
-    ],
-    characters: [
-        {
-            name: 'brimstone',
-            aliases: ['brim']
-        },
-        {
-            name: 'omen',
-            aliases: []
-        },
-        {
-            name: 'viper',
-            aliases: []
-        },
-        {
-            name: 'breach',
-            aliases: []
-        },
-        {
-            name: 'skye',
-            aliases: []
-        },
-        {
-            name: 'sova',
-            aliases: []
-        },
-        {
-            name: 'killjoy',
-            aliases: ['kj']
-        },
-        {
-            name: 'cypher',
-            aliases: []
-        },
-        {
-            name: 'jett',
-            aliases: []
-        },
-        {
-            name: 'phoenix',
-            aliases: []
-        },
-        {
-            name: 'reyna',
-            aliases: []
-        },
-        {
-            name: 'yoru',
-            aliases: []
-        },
-        {
-            name: 'raze',
-            aliases: []
-        },
-    ],
-    roles: [
-        {
-            name: 'entry',
-            aliases: []
-        },
-        {
-            name: 'lurker',
-            aliases: ['lurk']
-        },
-        {
-            name: "operator",
-            aliases: ['op', 'oper', 'awp']
-        },
-        {
-            name: 'controller',
-            aliases: ['']
-        },
-        {
-            name: 'initator',
-            aliases: ['']
-        },
-        {
-            name: 'duelist',
-            aliases: ['']
-        },
-        {
-            name: 'sentinel',
-            aliases: ['']
-        },
-    ],
-}
+  name: "valorant",
+  id: "1",
+  ranks: [
+    {
+      name: "bronze",
+      tiers: ["1", "2", "3"],
+      aliases: ["bronz", "br", "b"],
+    },
+    {
+      name: "silver",
+      tiers: ["1", "2", "3"],
+      aliases: ["silv", "sil", "s"],
+    },
+    {
+      name: "gold",
+      tiers: ["1", "2", "3"],
+      aliases: ["gl", "gd", "go", "g"],
+    },
+    {
+      name: "platinum",
+      tiers: ["1", "2", "3"],
+      aliases: ["plat", "p"],
+    },
+    {
+      name: "diamond",
+      tiers: ["1", "2", "3"],
+      aliases: ["dia", "d"],
+    },
+    {
+      name: "immortal",
+      tiers: ["1", "2", "3"],
+      aliases: ["immo", "imm", "i"],
+    },
+    {
+      bracket: "radiant",
+      tiers: ["1", "2", "3"],
+      aliases: ["rad", "r"],
+    },
+  ],
+  characters: [
+    {
+      name: "brimstone",
+      aliases: ["brim"],
+    },
+    {
+      name: "omen",
+      aliases: [],
+    },
+    {
+      name: "viper",
+      aliases: [],
+    },
+    {
+      name: "breach",
+      aliases: [],
+    },
+    {
+      name: "skye",
+      aliases: [],
+    },
+    {
+      name: "sova",
+      aliases: [],
+    },
+    {
+      name: "killjoy",
+      aliases: ["kj"],
+    },
+    {
+      name: "cypher",
+      aliases: [],
+    },
+    {
+      name: "jett",
+      aliases: [],
+    },
+    {
+      name: "phoenix",
+      aliases: [],
+    },
+    {
+      name: "reyna",
+      aliases: [],
+    },
+    {
+      name: "yoru",
+      aliases: [],
+    },
+    {
+      name: "raze",
+      aliases: [],
+    },
+  ],
+  roles: [
+    {
+      name: "entry",
+      aliases: [],
+    },
+    {
+      name: "lurker",
+      aliases: ["lurk"],
+    },
+    {
+      name: "operator",
+      aliases: ["op", "oper", "awp"],
+    },
+    {
+      name: "controller",
+      aliases: [],
+    },
+    {
+      name: "initator",
+      aliases: [],
+    },
+    {
+      name: "duelist",
+      aliases: [],
+    },
+    {
+      name: "sentinel",
+      aliases: [],
+    },
+  ],
+};
--- message_parse.js
+++ message_parse.js
@@ -13,26 +13,76 @@ const parseMessageFunction = (message) => {
   //Want to get the array of ranks...might not be how you are supposed to use this function
 
   // Hard coded for now
-  const hardCodedGame = 'valorant';
-
+  const hardCodedGame = "valorant";
   const game = getGame(hardCodedGame);
 
+  //ranks
   const arrRanks = game.ranks;
-
+  const rankArray = [];
   for (const rank of arrRanks) {
     if (lowercaseMessage.includes(rank.name)) {
-      console.log(rank.name);
+      rankArray.push(rank.name);
     }
   }
 
-  //Want to get the array of ranks...might not be how you are supposed to use this function
-
+  //roles
+  //"can rifle" is in one of the LFTs not sure if that is a role because it is in that section
   const arrRoles = game.roles;
-
+  const roleArray = [];
   for (const role of arrRoles) {
     if (lowercaseMessage.includes(role.name)) {
-      console.log(role.name);
+      roleArray.push(role.name);
+    } else {
+      const arrAlias = role.aliases;
+      for (const alias of arrAlias) {
+        if (
+          lowercaseMessage.includes(" " + alias + " ") ||
+          lowercaseMessage.includes(" " + alias + ",")
+        ) {
+          roleArray.push(role.name);
+        }
+      }
     }
+  }
+
+  //characters
+  //sage isn't in the character list...I see that in an LFT not sure if it is really a character
+  const arrChar = game.characters;
+
+  const characterArray = [];
+  for (const char of arrChar) {
+    if (lowercaseMessage.includes(char.name)) {
+      characterArray.push(char.name);
+    } else {
+      const arrAlias = char.aliases;
+      for (const alias of arrAlias) {
+        if (
+          lowercaseMessage.includes(" " + alias + " ") ||
+          lowercaseMessage.includes(" " + alias + ",")
+        ) {
+          characterArray.push(char.name);
+        }
+      }
+    }
+  }
+
+  //printing info
+  let printObj = { ranks: rankArray, roles: roleArray, chars: characterArray };
+  let i = 0;
+  if (printObj.ranks.length == 0) {
+    console.log("No ranks");
+  } else {
+    console.log("Ranks are: " + printObj.ranks);
+  }
+  if (printObj.roles.length == 0) {
+    console.log("No roles");
+  } else {
+    console.log("Roles are: " + printObj.roles);
+  }
+  if (printObj.chars.length == 0) {
+    console.log("No characters");
+  } else {
+    console.log("Characters are: " + printObj.chars);
   }
 };
 
